3c643b5e29923b2c8e3e6130f6a8ee0c8eac3d07
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-05-29 09:31:42 -0300

    Utilizando template, Data Binding con Curly Braces, ngFor para un array

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,8 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    template: `
+    <h2>Courses</h2>
+    {{title}}
+    <ul>
+        <li *ngFor="#course of courses">
+            {{course}}
+        </li>
+    </ul>
+    `
 })
 export class CoursesComponent {
-    
+    title: string = 'The title of course page';
+    courses: Array<string> = ['Course 1', 'Course 2', 'Course 3'];
 }
