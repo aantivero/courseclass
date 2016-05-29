43d1cf4439687d38ed92c8feb2c37d9da8e6ee07
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-05-29 08:47:58 -0300

    Iniciando un proyecto sencillo

--- src/app/courseclass.component.ts
+++ src/app/courseclass.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'courseclass-app',
-  templateUrl: 'courseclass.component.html',
-  styleUrls: ['courseclass.component.css']
+  template: '<h1>{{title}}</h1>'
 })
 export class CourseclassAppComponent {
   title = 'courseclass works!';
