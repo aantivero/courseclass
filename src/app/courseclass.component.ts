361c84bd0abdaa395f750e6efa0140a1106d1e8b
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-05-29 09:09:00 -0300

    Crear el primer component e importarlo en el component principal

--- src/app/courseclass.component.ts
+++ src/app/courseclass.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CoursesComponent} from './courses.component';
 
 @Component({
   moduleId: module.id,
   selector: 'courseclass-app',
-  template: '<h1>{{title}}</h1>'
+  template: '<h1>{{title}}</h1><courses></courses>',
+  directives: [CoursesComponent]
 })
 export class CourseclassAppComponent {
   title = 'courseclass works!';
